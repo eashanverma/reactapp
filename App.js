a269462b09fc9a0996d3bb84360680248de266c7
Eashan Verma <158998505+eashanverma@users.noreply.github.com>
2025-03-05 16:10:54 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This game is made by Eashan Verma (24251111)</h1>
+      <Game />
+    </div>
+  );
+}
+export default App;
